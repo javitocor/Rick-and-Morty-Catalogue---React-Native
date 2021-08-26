a81cc4c19f0bee5cd605101b42703fab34285d38
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-26 09:27:25 +0200

    add colors palette

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -2,14 +2,14 @@ export default {
   white: '#fff',
   border: '#E2E2E2',
   text: '#343434',
-  blue: '#4F6D7A',
+  blue: '#62a4ab',
   textLight: '#797979',
   offWhite: '#F0F0F0',
   black: 'black',
-  yellow: 'yellow',
+  yellow: '#F0E14A',
   grey:'grey',
-  brown: 'brown',
-  green: 'green',
-  pink: 'pink',
-  purple: 'purple'
+  brown: '#44281D',
+  green: '#97CE4C',
+  pink: '#E4A788',
+  purple: '#E89AC7'
 };

58bf44a1b893181f2ab2187fb63d9a3fdb5e42d8
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-26 08:20:24 +0200

    implement api calls

---
+++ App/helpers/APIcall.js
@@ -0,0 +1,91 @@
+/* eslint-disable consistent-return */
+import 'regenerator-runtime/runtime';
+import { BASE_URL } from '../constants/constants';
+import * as fromEpisodes from '../actions/episodes';
+import * as fromLocations from '../actions/locations';
+import * as fromCharacters from '../actions/characters';
+import * as fromSearches from '../actions/search';
+
+export const AllCall = (route) => async dispatch => {
+  const Url = `${BASE_URL + route}`;
+  
+  try {
+    if(route==='episode'){
+      dispatch(fromEpisodes.getAllEpisodesPending());
+    } else if (route==='location') {
+      dispatch(fromLocations.getAllLocationsPending());
+    } else if (route==='character'){
+      dispatch(fromCharacters.getAllCharactersPending());
+    }         
+
+    const response = await fetch(Url, { mode: 'cors'});
+    const data = await response.json();
+    if(route==='episode'){
+      dispatch(fromEpisodes.getAllEpisodes(data));
+    } else if (route==='location') {
+      dispatch(fromLocations.getAllLocations(data));
+    } else if (route==='character'){
+      dispatch(fromCharacters.getAllCharacters(data));
+    }     
+    return data;
+  } catch (error) {
+    if(route==='episode'){
+      dispatch(fromEpisodes.getAllEpisodesError(error));
+    } else if (route==='location') {
+      dispatch(fromLocations.getAllLocationsError(error));
+    } else if (route==='character'){
+      dispatch(fromCharacters.getAllCharactersError(error));
+    } 
+  }
+};
+
+export const SingleCall = (route, id) => async dispatch => {
+  const Url = `${BASE_URL + route}`;
+  try {
+    if(route==='episode'){
+      dispatch(fromEpisodes.getSingleEpisodePending());
+    } else if (route==='location') {
+      dispatch(fromLocations.getSingleLocationPending());
+    } else if (route==='character'){
+      dispatch(fromCharacters.getSingleCharacterPending());
+    }
+
+    const response = await fetch(`${Url}/${id}`, { mode: 'cors' });
+    const data = await response.json();
+    if(route==='epsiode'){
+      dispatch(fromEpisodes.getSingleEpisode(data));
+    } else if (route==='location') {
+      dispatch(fromLocations.getSingleLocation(data));
+    } else if (route==='character'){
+      dispatch(fromCharacters.getSingleCharacter(data));
+    }
+    return data;
+  } catch (error) {
+    if(route==='episode'){
+      dispatch(fromEpisodes.getSingleEpisodeError(error));
+    } else if (route==='location') {
+      dispatch(fromLocations.getSingleLocationError(error));
+    } else if (route==='character'){
+      dispatch(fromCharacters.getSingleCharacterError(error));
+    }   
+  }
+};
+
+
+export const SearchCall = (resource, param, value) => async dispatch => {
+  const Url = `${BASE_URL + resource}`;
+
+  try{
+    dispatch(fromSearches.getSearchesPending());
+
+    const response = await fetch(`${Url}/?${param}=${value}`, { mode: 'cors' });
+    const data = await response.json();
+
+    dispatch(fromSearches.getSearches(data, resource, param, value));
+
+    return data;
+  } catch (error) {
+    dispatch(fromSearches.getSearchesError(error));
+  }
+  
+};

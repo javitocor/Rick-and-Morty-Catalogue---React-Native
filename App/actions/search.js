e296d228307122d92121b641b214775aa7130ae4
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-26 07:53:50 +0200

    modify search actions

--- App/actions/search.js
+++ App/actions/search.js
@@ -2,7 +2,7 @@ import {
   GET_SEARCHES, GET_SEARCHES_PENDING, GET_SEARCHES_ERROR
 } from '../constants/constants';
 
-export const getSearches = (searchResults, param, value,resource) => ({
+export const getSearches = (searchResults, resource, param, value) => ({
   type: GET_SEARCHES,
   pages: searchResults.info.pages,
   next: searchResults.info.next,
